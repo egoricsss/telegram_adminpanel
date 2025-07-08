a203556abe50a40abe2d21a0b6f6ea950fce7a5d
egoric <egor-syrov@bk.ru>
2025-07-08 23:11:34 +0300

    update base filter

--- src/command_handlers/base.py
+++ src/command_handlers/base.py
@@ -1,11 +1,12 @@
 from aiogram import Router
 from aiogram.types import Message
+from utils import IsAdmin
 
 __all__ = ["router"]
 
 router = Router()
 
 
-@router.message()
+@router.message(IsAdmin())
 async def echo(message: Message) -> None:
     await message.answer(message.text)
