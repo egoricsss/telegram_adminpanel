cfae6bc30913e3f1a751b8784456aa53e7957501
egoric <egor-syrov@bk.ru>
2025-07-09 10:19:58 +0300

    add accept state

--- src/command_handlers/addclient.py
+++ src/command_handlers/addclient.py
@@ -4,7 +4,13 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import FSInputFile, InputMediaDocument, Message
+from aiogram.types import (
+    FSInputFile,
+    InputMediaDocument,
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+)
 
 from utils import run_wireguard_cmd
 
@@ -15,6 +21,7 @@ router = Router()
 
 class AddClientState(StatesGroup):
     name = State()
+    accept = State()
 
 
 @router.message(Command("addclient"))
@@ -25,19 +32,33 @@ async def addclient(message: Message, state: FSMContext) -> None:
 
 @router.message(AddClientState.name)
 async def process_added_clientname(message: Message, state: FSMContext) -> None:
-    await state.clear()
     client_name = message.text.strip()
-    run_wireguard_cmd(["--addclient", client_name])
+    await state.update_data(name=client_name)
+    await state.set_state(AddClientState.accept)
+    await message.answer(
+        f"Are you sure to add client: {client_name}?",
+        reply_markup=ReplyKeyboardMarkup(
+            [KeyboardButton(text="Yes"), KeyboardButton(text="No")]
+        ),
+    )
 
-    conf_path = Path.home() / f"{client_name}.conf"
-    png_path = Path.home() / f"{client_name}.png"
 
-    conf_file = FSInputFile(conf_path, filename=f"{client_name}.conf")
-    png_file = FSInputFile(png_path, filename=f"{client_name}.png")
+@router.message(AddClientState.accept)
+async def add_client(message: Message, state: FSMContext) -> None:
+    if message.text == "Yes":
+        data = await state.get_data()
+        client_name = data["name"]
+        run_wireguard_cmd(["--addclient", client_name])
+        conf_path = Path.home() / f"{client_name}.conf"
+        png_path = Path.home() / f"{client_name}.png"
 
-    media = [
-        InputMediaDocument(media=conf_file, caption=f"{client_name}`s config"),
-        InputMediaDocument(media=png_file, caption=f"{client_name}`s QR-code"),
-    ]
+        conf_file = FSInputFile(conf_path, filename=f"{client_name}.conf")
+        png_file = FSInputFile(png_path, filename=f"{client_name}.png")
 
-    await message.answer_media_group(media)
+        media = [
+            InputMediaDocument(media=conf_file, caption=f"{client_name}`s config"),
+            InputMediaDocument(media=png_file, caption=f"{client_name}`s QR-code"),
+        ]
+
+        await message.answer_media_group(media)
+    await state.clear()
--- src/command_handlers/removeclient.py
+++ src/command_handlers/removeclient.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 from utils import run_wireguard_cmd
 
@@ -24,7 +24,22 @@ async def removeclient(message: Message, state: FSMContext) -> None:
 
 @router.message(RemoveClientState.name)
 async def process_removed_clientname(message: Message, state: FSMContext) -> None:
-    await state.set_state(RemoveClientState.accept)
     client_name = message.text.strip()
-    wireguard_response = run_wireguard_cmd(["--removeclient", client_name])
-    await message.answer(f"<code>{wireguard_response}</code>")
+    await state.update_data(name=client_name)
+    await state.set_state(RemoveClientState.accept)
+    await message.answer(
+        f"Are you sure to remove client: {client_name}?",
+        reply_markup=ReplyKeyboardMarkup(
+            [KeyboardButton(text="Yes"), KeyboardButton(text="No")]
+        ),
+    )
+
+
+@router.message(RemoveClientState.accept)
+async def remove_client(message: Message, state: FSMContext) -> None:
+    if message.text == "Yes":
+        data = await state.get_data()
+        client_name = data["name"]
+        wireguard_response = run_wireguard_cmd(["--removeclient", client_name])
+        await message.answer(f"<code>{wireguard_response}</code>")
+    await state.clear()
